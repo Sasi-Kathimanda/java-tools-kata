d2d5d1d132b55033f79b8ef68039ed6ef03e4d50
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-04-28 20:27:50 +0100

    [Jackson]-adding date to the person and by default its long epoch which not human friendly to read

--- lib/src/main/java/tools/kata/jackson/Person.java
+++ lib/src/main/java/tools/kata/jackson/Person.java
@@ -2,15 +2,19 @@ package tools.kata.jackson;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.util.Date;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Person {
     private String name;
     private int age;
     private Residence residence;
+    private Date dob;
 
-    public Person(String name, int age) {
+    public Person(String name, int age , Date dob) {
         this.name = name;
         this.age = age;
+        this.dob = dob;
     }
 
     public Person() {
@@ -26,5 +30,9 @@ public class Person {
 
     public Residence getResidence() {
         return residence;
+    }
+
+    public Date getDob() {
+        return dob;
     }
 }
--- lib/src/test/java/tools/kata/jackson/JsonWriterTest.java
+++ lib/src/test/java/tools/kata/jackson/JsonWriterTest.java
@@ -4,12 +4,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JsonWriterTest {
-    public static final String PERSON_JSON = "{\"name\":\"Sas\",\"age\":18}";
+    public static final String PERSON_JSON = "{\"name\":\"Sas\",\"age\":18,\"dob\":470830530000}";
     JsonWriter sut;
-    Person person = new Person("Sas", 18);
+    Person person = new Person("Sas", 18 , Date.from(Instant.parse("1984-12-02T10:15:30.00Z")));
 
     @BeforeEach
     void setUp() {
