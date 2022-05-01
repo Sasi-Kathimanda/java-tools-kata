55d33d6f6de69cb6143344dd11e45cb053282b93
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-05-01 23:05:00 +0100

    [Junit4]- verify assert throws of the junit 4

--- lib/src/test/java/tools/kata/junit4/ExceptedExceptionRuleTest.java
+++ lib/src/test/java/tools/kata/junit4/ExceptedExceptionRuleTest.java
@@ -4,6 +4,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import static org.junit.Assert.assertThrows;
+
 public class ExceptedExceptionRuleTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -19,5 +21,11 @@ public class ExceptedExceptionRuleTest {
         thrown.expect(NullPointerException.class);
         thrown.expectMessage("Null Pointer Problem");
         throw new NullPointerException("Null Pointer Problem");
+    }
+
+    @Test
+    public void testExceptionWithAssertThrows() {
+        assertThrows(NumberFormatException.class, () -> Integer.parseInt("Sas"));
+        assertThrows(IllegalArgumentException.class, () -> Integer.parseInt("Sas"));
     }
 }
