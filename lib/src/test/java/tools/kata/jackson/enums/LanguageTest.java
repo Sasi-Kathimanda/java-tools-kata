8f388586c9b2d93e0025c09a39de756b447ab5c3
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-05-04 19:50:52 +0100

    [ENUM]- verify the individual values from the values method

--- lib/src/test/java/tools/kata/jackson/enums/LanguageTest.java
+++ lib/src/test/java/tools/kata/jackson/enums/LanguageTest.java
@@ -3,7 +3,7 @@ package tools.kata.jackson.enums;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LanguageTest {
     Language sut;
@@ -15,6 +15,9 @@ class LanguageTest {
     @Test
     void values() {
         assertEquals(3, Language.values().length);
+        assertEquals(Language.TELUGU, Language.values()[0]);
+        assertEquals(Language.ENGLISH, Language.values()[1]);
+        assertEquals(Language.TAMIL, Language.values()[2]);
     }
 
     @Test
