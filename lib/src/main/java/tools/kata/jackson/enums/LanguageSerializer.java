31628b4a798846d99e25344b0cee0d6e8d49de87
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-05-07 08:39:49 +0100

    [ENUM]- adding test for the lang serializer to verify serialization func

--- lib/src/main/java/tools/kata/jackson/enums/LanguageSerializer.java
+++ lib/src/main/java/tools/kata/jackson/enums/LanguageSerializer.java
@@ -1,20 +1,12 @@
 package tools.kata.jackson.enums;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 import java.io.IOException;
 
-public class LanguageSerializer extends StdSerializer<Language> {
-
-    protected LanguageSerializer(Class<Language> t) {
-        super(t);
-    }
-
-    public LanguageSerializer() {
-        super(Language.class);
-    }
+public class LanguageSerializer extends JsonSerializer<Language> {
 
     @Override
     public void serialize(Language value, JsonGenerator gen, SerializerProvider provider) throws IOException {
--- lib/src/test/java/tools/kata/jackson/enums/LanguageSerializerTest.java
+++ lib/src/test/java/tools/kata/jackson/enums/LanguageSerializerTest.java
@@ -1,23 +1,25 @@
 package tools.kata.jackson.enums;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
 class LanguageSerializerTest {
     LanguageSerializer sut;
+    ObjectMapper mapper;
 
     @BeforeEach
     void setUp() {
         sut = new LanguageSerializer();
+        mapper = new ObjectMapper();
     }
 
     @Test
-    @Disabled
     void serializeWillSpecifyTheStructureOfEnumYouLike() throws IOException {
-        //TODO: add mocking ?
-       // sut.serialize(Language.TELUGU, null, null);
+        String actual = mapper.writeValueAsString(Language.TELUGU);
+        Assertions.assertEquals("{\"prefix\":\"TEL\",\"message\":\"Namaskaram\"}", actual);
     }
 }
