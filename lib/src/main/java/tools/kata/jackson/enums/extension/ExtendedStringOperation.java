29791b956e4817bf33993ab7e2ce404b42e1c33e
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-05-10 06:38:54 +0100

    [ENUM]- implementing the md5 hash gen

--- lib/src/main/java/tools/kata/jackson/enums/extension/ExtendedStringOperation.java
+++ lib/src/main/java/tools/kata/jackson/enums/extension/ExtendedStringOperation.java
@@ -1,10 +1,12 @@
 package tools.kata.jackson.enums.extension;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 public enum ExtendedStringOperation implements StringOperation {
     MD5_ENCODE("encode the text to md5 string"){
         @Override
         public String apply(String input) {
-            return "";
+            return DigestUtils.md5Hex(input);
         }
     },
     BASE64_ENCODE("encode the text to base 64 encoded string") {
