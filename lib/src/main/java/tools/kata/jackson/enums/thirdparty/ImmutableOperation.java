0e92b15caa7f9a28c290e55683052eea5edfa342
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-05-11 06:31:13 +0100

    [ENUM]- implementing the logic for reversing the case

--- lib/src/main/java/tools/kata/jackson/enums/thirdparty/ImmutableOperation.java
+++ lib/src/main/java/tools/kata/jackson/enums/thirdparty/ImmutableOperation.java
@@ -10,7 +10,20 @@ public class ImmutableOperation {
     static {
         OPERATION_MAP = new EnumMap<>(RemoteEnum.class);
         OPERATION_MAP.put(RemoteEnum.LOWERCASE, String::toLowerCase);
-        OPERATION_MAP.put(RemoteEnum.INVERT_CASE, s -> "");
+        OPERATION_MAP.put(RemoteEnum.INVERT_CASE, ImmutableOperation::reverseCase);
+    }
+
+    private static String reverseCase(final String text) {
+        var chars = text.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (Character.isUpperCase(c)) {
+                chars[i] = Character.toLowerCase(c);
+            } else if (Character.isLowerCase(c)) {
+                chars[i] = Character.toUpperCase(c);
+            }
+        }
+        return new String(chars);
     }
 
     private ImmutableOperation() {
