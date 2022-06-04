e6c038a789bba65607b6c563f4364ec0c24d2dee
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-06-04 23:56:04 +0100

    [BAU]-removing wild card imports

--- lib/src/test/java/tools/kata/jackson/enums/extension/BasicStringOperationTest.java
+++ lib/src/test/java/tools/kata/jackson/enums/extension/BasicStringOperationTest.java
@@ -2,7 +2,8 @@ package tools.kata.jackson.enums.extension;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class BasicStringOperationTest {
 
