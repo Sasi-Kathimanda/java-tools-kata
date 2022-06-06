a828bde7c05c663f2f0613e8f92ea78fb221f5cd
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-06-06 21:32:57 +0100

    [JUNIT5]- passing test for valid person object

--- lib/src/main/java/tools/kata/junit5/PersonValidator.java
+++ lib/src/main/java/tools/kata/junit5/PersonValidator.java
@@ -2,11 +2,14 @@ package tools.kata.junit5;
 
 import tools.kata.jackson.Person;
 
+import java.util.regex.Pattern;
+
 public class PersonValidator {
 
     public boolean validPerson(Person person) {
-        if (person != null)
-            throw new IllegalArgumentException("fail me");
-        return false;
+        var pattern = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        if (pattern.matcher(person.getName()).find())
+            throw new IllegalArgumentException("invalid symbols or number in name" + person.getName());
+        return true;
     }
 }
