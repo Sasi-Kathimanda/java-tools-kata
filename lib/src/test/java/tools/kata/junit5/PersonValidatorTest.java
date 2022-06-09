53a9f0f6658af00e1c4e0b5ab03af1993ddcbdda
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-06-09 21:30:28 +0100

    [Junit5]-adding invalid param resolver failing test

--- lib/src/test/java/tools/kata/junit5/PersonValidatorTest.java
+++ lib/src/test/java/tools/kata/junit5/PersonValidatorTest.java
@@ -18,4 +18,12 @@ class PersonValidatorTest {
             Assertions.assertDoesNotThrow(() -> new PersonValidator().validPerson(person));
         }
     }
+    @Nested
+    @ExtendWith(InvalidPersonResolver.class)
+    class InValidPersonValidator {
+        @RepeatedTest(value = 10)
+        void validatePersonName(Person person) {
+            Assertions.assertDoesNotThrow(() -> new PersonValidator().validPerson(person));
+        }
+    }
 }
