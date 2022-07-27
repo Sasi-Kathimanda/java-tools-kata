1b5e6e254752c6f7a7454b41fb268b51ff9f858f
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-07-27 20:05:34 +0100

    [BAU]-removed wildcard import

--- lib/src/test/java/tools/kata/jackson/enums/extension/ExtendedStringOperationTest.java
+++ lib/src/test/java/tools/kata/jackson/enums/extension/ExtendedStringOperationTest.java
@@ -3,7 +3,8 @@ package tools.kata.jackson.enums.extension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class ExtendedStringOperationTest {
     @BeforeEach
