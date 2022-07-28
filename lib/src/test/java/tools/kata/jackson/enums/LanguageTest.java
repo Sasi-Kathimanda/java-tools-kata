3012a39fa2a6589e5a166e3f53d337cee25e4adc
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-07-28 22:15:10 +0100

    [BAU]-remove wildcard import

--- lib/src/test/java/tools/kata/jackson/enums/LanguageTest.java
+++ lib/src/test/java/tools/kata/jackson/enums/LanguageTest.java
@@ -3,7 +3,10 @@ package tools.kata.jackson.enums;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class LanguageTest {
     Language sut;
