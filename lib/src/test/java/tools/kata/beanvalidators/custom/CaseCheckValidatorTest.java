153b9a4b01b9e7e72aaae5461ce5b8f77d0c8909
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-11-12 23:16:05 +0000

    [BEAN-VALIDATORS] - adding failing test for custom validation

---
+++ lib/src/test/java/tools/kata/beanvalidators/custom/CaseCheckValidatorTest.java
@@ -0,0 +1,34 @@
+package tools.kata.beanvalidators.custom;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tools.kata.beanvalidators.Car;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaseCheckValidatorTest {
+    private static Validator validator;
+    private static final LocalDate regDate = LocalDate.of(2017, 3, 19);
+
+    @BeforeEach
+    void setUp() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Test
+    void isValid() {
+        //given
+        var car = new Car("Audi A7", "RV07YFG", 4, regDate, "sk@sk.com", BigDecimal.ZERO, "(999)888-1234");
+        //when
+        var violations = validator.validate(car);
+        //then
+        assertEquals(1, violations.size());
+        assertEquals("", violations.iterator().next().getMessage());
+    }
+}
