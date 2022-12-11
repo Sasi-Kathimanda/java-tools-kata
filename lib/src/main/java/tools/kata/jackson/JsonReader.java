ebba1fd8b456e9e0d9307d5d0460549c4199b63b
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-12-11 00:09:59 +0000

    [BAU] - checkstyle increasing linelength to 140 and fixed jso reader warnings

--- lib/src/main/java/tools/kata/jackson/JsonReader.java
+++ lib/src/main/java/tools/kata/jackson/JsonReader.java
@@ -3,11 +3,9 @@ package tools.kata.jackson;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.Map;
 
 public class JsonReader {
