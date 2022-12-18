164c89c53c990678587b8664960c3f3355c50fb1
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-12-18 14:31:52 +0000

    [BAU] - checkstyle import order

--- lib/src/main/java/tools/kata/jackson/enums/LanguageSerializerWithMaskedMsg.java
+++ lib/src/main/java/tools/kata/jackson/enums/LanguageSerializerWithMaskedMsg.java
@@ -3,7 +3,6 @@ package tools.kata.jackson.enums;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-
 import java.io.IOException;
 
 public class LanguageSerializerWithMaskedMsg extends JsonSerializer<Language> {
