a9a0e552b2a7226fe01c08f44c5e019e1249d2ed
Sasi Kathimanda <ksasikiranraja@gmail.com>
2024-03-12 20:28:29 +0000

    working version of the docker hello

--- lib/src/main/java/tools/kata/Library.java
+++ lib/src/main/java/tools/kata/Library.java
@@ -4,4 +4,8 @@ public class Library {
     public boolean someLibraryMethod() {
         return true;
     }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
